643f877b125fce00cdfe1fa23d3f171e41557662
Remon <remmony@gmail.com>
2018-07-05 06:45:30 +0200

    add messages

---
+++ message.py
@@ -0,0 +1,21 @@
+import random
+
+word_list =["Whatever you are, be a good one.", 
+	"If you dream it, you can do it.", "Never, never, never give up.", 
+	"Don't wait. The time will never be just right.", 
+	"If not us, who? If not now, when?", "Everything you can imagine is real.",
+	 "I can, therefore I am.", "Remember no one can make you feel inferior without your consent.", "Turn your wounds into wisdom.",
+	  "Wherever you go, go with all your heart.", "Do what you can, with what you have, where you are.", "Hope is a waking dream.",
+	   "Action is the foundational key to all success.", "Do one thing every day that scares you.", 
+	   "You must do the thing you think you cannot do.", "Life is trying things to see if they work.",
+	    "Don't regret the past, just learn from it.", "Believe you can and you're halfway there.", "Live what you love.",
+	     "The power of imagination makes us infinite.", "May you live every day of your life.", 
+	     "Eighty percent of success is showing up.", "To be the best, you must be able to handle the worst.",
+	      "A jug fills drop by drop.", "The obstacle is the path.", "The best revenge is massive success.",
+	       "The best way out is always through.", "If you have never failed you have never lived.", 
+	       "Hope is the heartbeat of the soul.", "Ever tried. Ever failed. No matter. Try Again. Fail again. Fail better.",
+	        "All you need is love.", "It does not matter how slowly you go as long as you do not stop.", 
+	        "It is never too late to be what you might have been.", "We become what we think about.",
+	        "An obstacle is often a stepping stone.", "Dream big and dare to fail.", "Men are born to succeed, not fail."]
+
+print(random.choice(word_list))
